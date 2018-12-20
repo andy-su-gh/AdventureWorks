f3e794973276e7d547780bd628e0cb77fbda070d
andysu8685 <andy-su@163.com>
2018-12-20 13:37:23 +0800

    add log4Net

--- AdventureWorks.Web/Global.asax.cs
+++ AdventureWorks.Web/Global.asax.cs
@@ -10,12 +10,16 @@ namespace AdventureWorks.Web
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        log4net.ILog logger = log4net.LogManager.GetLogger("helloWorld.Logging");
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            logger.Info("Now: " + DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss"));
+            logger.Info("HostName: " + System.Net.Dns.GetHostName());
         }
     }
 }
